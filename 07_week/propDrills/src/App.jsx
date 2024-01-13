f8809609c47593f4309768522ae51e21391a3f19
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-13 20:43:33 +0530

    context API

--- 07_week/propDrills/src/App.jsx
+++ 07_week/propDrills/src/App.jsx
@@ -1,29 +1,36 @@
 import { useState } from "react"
+import { CountContext } from "./context";
+import { useContext } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   
+  // wrap anything that wants to use the value inside a Provider
   return (
     <div>
-      <Count count={count} setCount={setCount} />
+      <CountContext.Provider value = {count}>
+      <Count setCount={setCount} />
+      </CountContext.Provider>
     </div>
   )
 }
 
-function Count({count, setCount}) {
+function Count({setCount}) {
   return <div>
-    <CountRenderer count={count} />
-    <Buttons count={count} setCount={setCount} />
+    <CountRenderer />
+    <Buttons setCount={setCount} />
   </div>
 }
 
-function CountRenderer({count}) {
+function CountRenderer() {
+  const count = useContext(CountContext)
   return <div>
     {count}
   </div>
 }
 
-function Buttons({count, setCount}) {
+function Buttons({setCount}) {
+  const count = useContext(CountContext)
   return <div>
     <button onClick={() => {
       setCount(count + 1)
