39a4f65ff3c78b4923f944ffc790a00ea0707b89
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-13 20:10:04 +0530

    react routing

--- 02_week/02_express/01_basicHttp.js
+++
@@ -1,9 +0,0 @@
-// create basic http server using basic http server module
-
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-    res.write("Hey we are sending this response to you......")
-    res.end();
-})
-server.listen(8080)
--- 07_week/reactRouting/src/App.jsx
+++ 07_week/reactRouting/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Dashboard = lazy(() => import('./components/Dashboard'))
+const Landing = lazy(() => import('./components/Landing'))
 
+function App() {
+  
+  // Suspense API
   return (
-    <>
-      
-    </>
+    <div>
+      <BrowserRouter>
+        <Appbar />
+        <Routes>
+            <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>} />
+            <Route path="/" element={<Suspense fallback={"loading..."}><Landing /></Suspense>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
+}
+
+function Appbar() {
+  const navigate = useNavigate();
+
+  return <div>
+      <div>
+        <button onClick={() => {
+          navigate("/");
+        }}>Landing page</button>
+
+        <button onClick={() => {
+          navigate("/dashboard");
+        }}>Dashboard</button>
+
+      </div>
+  </div>
 }
 
 export default App
