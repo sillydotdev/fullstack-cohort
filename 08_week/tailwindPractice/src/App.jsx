bba411506ff351d5dd4832a1a310d839bdeb7d3c
malik-kamran-mushtaq <kamran145422@gmail.com>
2024-01-20 20:44:38 +0530

    tailwind css

--- 08_week/tailwindPractice/src/App.jsx
+++ 08_week/tailwindPractice/src/App.jsx
@@ -6,8 +6,8 @@ import { RevenueCard } from './components/RevenueCard'
 
 function App() {
   return (
-    // <>
-    //   {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
+    <>
+      {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
     //     <div style={{backgroundColor: "red"}}>Red</div>
     //     <div style={{backgroundColor: "green"}}>Green</div>
     //     <div style={{backgroundColor: "blue"}}>Blue</div>
@@ -19,33 +19,33 @@ function App() {
     //     <div className='bg-blue-500'>Blue</div>
     //   </div> */}
 
-    //   {/* <div className='flex'>
+      {/* <div className='flex'>
     //     <div className='bg-red-500 w-[50%]'>My name is Kamran</div>
     //     <div className='bg-yellow-500 w-[30%]'>My name is Danish</div>
     //     <div className='bg-green-500 w-[20%]'>My name is Saqib</div>
     //   </div> */}
 
-    //   {/* <div className='grid grid-cols-10'>
+      {/* <div className='grid grid-cols-10'>
     //     <div className='bg-red-500 col-span-5'>My name is Kamran</div>
     //     <div className='bg-yellow-500 col-span-3'>My name is Danish</div>
     //     <div className='bg-green-500 col-span-2'>My name is Saqib</div>
     //   </div> */}
 
-    //   {/* <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
-    //     <div className='bg-red-500'>My name is Kamran</div>
-    //     <div className='bg-yellow-500'>My name is Danish</div>
-    //     <div className='bg-green-500'>My name is Saqib</div>
-    //     <div className='bg-green-500'>My name is Ashraf</div>
-    //     <div className='bg-green-500'>My name is Burhan</div>
-    //   </div>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5'>
+        <div className='bg-red-500'>My name is Kamran</div>
+        <div className='bg-yellow-500'>My name is Danish</div>
+        <div className='bg-green-500'>My name is Saqib</div>
+        <div className='bg-green-500'>My name is Ashraf</div>
+        <div className='bg-green-500'>My name is Burhan</div>
+      </div>
 
-    //   <div className='bg-red-500 md:bg-blue-500'>
-    //     Hi there
-    //   </div> */}
-    // </>
-    <div className=''>
-      <RevenueCard />
-    </div>
+      <div className='bg-red-500 md:bg-blue-500'>
+        Hi there
+      </div>
+    </>
+  //   <div className=''>
+  //     <RevenueCard />
+  //   </div>
   )
 }
 
